1a2c4033c288ad9dd4feac0f5f86757e0967df35
KDonnelly33 <kevin.donnelly3@gmail.com>
2023-12-21 16:14:20 -0600

    restarted logic and how to handle the problem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,54 @@
+import { useState } from "react";
 
+function App() {
+  const [frames, setFrames] = useState(Array(10).fill([null, null]));
+  const renderRoll = (roll) => {
+    switch (roll) {
+      case 10:
+        return "X";
+      case 0:
+        return "-";
+      default:
+        return roll;
+    }
+  }
+  const totalScore = () => {
 
-function App() { 
-  const frames = Array.from({ length: 10 }, (_, index) => index + 1);
-
+  }
+  const handleRollClick = (pins) => {
+    console.log(pins);
+  }
   return (
     <>
-      {frames.map((frame, index) => (
-        <div key={index}>
-          <h2>{frame}</h2>
-          {index === 9 ? (  
-            <>
-            <div>
-            <input />
-            <input />
-            <input />
-            <button>Submit</button>
+      <h1>Bowling Scoreboard</h1>
+      <div>
+        {frames.map((frame, frameIndex) => {
+          return (
+            <div key={frameIndex}>
+              <h2>{frameIndex + 1}</h2>
+              {frame.map((roll, rollIndex) => {
+                return (
+                  <div key={rollIndex}>
+                    <h3>{renderRoll(roll)}</h3>
+                  </div>
+                );
+              })}
             </div>
-            <p>score</p>
-            </>
-          ) : (
-            <>
-            <div>
-             <input />
-              <input />
-              <button>Submit</button>
-            </div>
-            <p>score</p>
-            </>
-          )}
-        </div>
-      ))}
+          );
+        })}
+      </div>
+      <div>
+        total: {totalScore()}
+      </div>
+      <div>
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "/", "X"].map((pins) => {
+          return (
+            <button key={pins} onClick={() => handleRollClick(pins)}>
+              {pins}
+            </button>
+          );
+        })}
+      </div>
     </>
   );
 }
